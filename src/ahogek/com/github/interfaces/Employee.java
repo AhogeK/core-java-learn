d194d57269781ad0feaefdac813b6084ed59728e
AhogeK <ahogek@gmail.com>
2022-06-08 12:51:48 +0800

    feat(interfaces): Comparable 接口

---
+++ src/ahogek/com/github/interfaces/Employee.java
@@ -0,0 +1,71 @@
+package ahogek.com.github.interfaces;
+
+import java.util.Objects;
+
+/**
+ * @author AhogeK ahogek@gmail.com
+ * @since 2022-06-08 12:44:10
+ */
+public class Employee implements Comparable<Employee> {
+
+    private final String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return Double.compare(o.salary, salary);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) o;
+
+        if (Double.compare(employee.salary, salary) != 0) {
+            return false;
+        }
+        return Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/ahogek/com/github/interfaces/EmployeeSortTest.java
@@ -0,0 +1,27 @@
+package ahogek.com.github.interfaces;
+
+import java.util.Arrays;
+
+/**
+ * This program demonstrates the use of the {@link Comparable} interface
+ *
+ * @author AhogeK ahogek@gmail.com
+ * @since 2022-06-08 12:48:09
+ */
+public class EmployeeSortTest {
+
+    public static void main(String[] args) {
+        var staff = new Employee[3];
+
+        staff[0] = new Employee("Harry Hacker", 35000);
+        staff[1] = new Employee("Carl Cracker", 75000);
+        staff[2] = new Employee("Tony Tester", 38000);
+
+        Arrays.sort(staff);
+
+        // print out information about all Employee objects
+        for (Employee e : staff) {
+            System.out.println(e);
+        }
+    }
+}
