2488c9cfba383df95750bc0dd540a36aa4a93eb5
AhogeK <ahogek@gmail.com>
2022-07-11 22:59:59 +0800

    feat(v1ch02): first sample

---
+++ src/cn/ahogek/vonecthree/FirstSample.java
@@ -0,0 +1,14 @@
+package cn.ahogek.vonecthree;
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ *
+ * @author AhogeK ahogek@gmail.com
+ * @since 2022-07-11 12:05:04
+ */
+public class FirstSample {
+
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
