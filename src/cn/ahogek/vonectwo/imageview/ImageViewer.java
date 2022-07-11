4b3ab6434fdd27513f25970c6b07fee7ab3ce14e
AhogeK <ahogek@gmail.com>
2022-07-11 11:59:31 +0800

    feat(v1ch02): Image viewer

--- src/cn/ahogek/vonectwo/imageview/ImageViewer.java
+++ src/cn/ahogek/vonectwo/imageview/ImageViewer.java
@@ -1,8 +1,66 @@
 package cn.ahogek.vonectwo.imageview;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 /**
+ * A program for viewing images.
+ *
  * @author AhogeK cn@gmail.ahogek
  * @since 2022-07-10 00:29:04
  */
 public class ImageViewer {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var frame = new ImageViewerFrame();
+            frame.setTitle("ImageViewer");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+/**
+ * A frame with a label to show an image.
+ */
+class ImageViewerFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public ImageViewerFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        // use a label to display the images
+        var label = new JLabel();
+        add(label);
+
+        // set up the file chooser;
+        var chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+
+        // set up the menu bar
+        var menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        var menu = new JMenu("File");
+        menuBar.add(menu);
+
+        var openItem = new JMenuItem("Open");
+        menu.add(openItem);
+
+        openItem.addActionListener(event -> {
+            // show file chooser dialog
+            int result = chooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String name = chooser.getSelectedFile().getPath();
+                label.setIcon(new ImageIcon(name));
+            }
+        });
+
+        var exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(event -> System.exit(0));
+    }
 }
